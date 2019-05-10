605b8fde291a3e1d9fc451ed7bfdc69a25e7bfd3
Abhijith-R <abhr@cisco.com>
2019-05-10 16:10:49 +0530

    Update createDB.php

--- backend_php/createDB.php
+++ backend_php/createDB.php
@@ -1,5 +1,4 @@
-/*
-Copyright (c) 2019 Cisco and/or its affiliates.
+/*Copyright (c) 2019 Cisco and/or its affiliates.
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
 License at
@@ -10,8 +9,7 @@ the License. All rights not expressly granted by the License are
 reserved. Unless required by applicable law or agreed to separately in
 writing, software distributed under the License is distributed on an "AS
 IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied.
-*/
+or implied.*/
 
 <?php
 $link = new mysqli("127.0.0.1", "root", "newpassword","mytestdb");
